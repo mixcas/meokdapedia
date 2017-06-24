0e1d03e482c2324eef62b32ed114db6118e9eba8
cas <cas@interglobal.vision>
2017-06-23 21:19:36 -0500

    add workign stuff

--- src/home/index.js
+++ src/home/index.js
@@ -19,6 +19,11 @@ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui
 import FlatButton from 'material-ui/FlatButton'
 import Paper from 'material-ui/Paper'
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+if (typeof window !== 'undefined') {
+	injectTapEventPlugin();
+}
+
 import DishesList from '../../components/DishesList'
 
 import dishes from '../../public/dishes'
@@ -67,74 +72,3 @@ class HomePage extends React.Component {
 }
 
 export default HomePage;
-
-/*
-import injectTapEventPlugin from 'react-tap-event-plugin';
-if (typeof window !== 'undefined') {
-	injectTapEventPlugin();
-}
-
-import DishesList from '../components/DishesList'
-
-import dishes from '../static/dishes'
-
-export default class extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.handleInputText = this.handleInputText.bind(this);
-
-		this.state = {
-			dishes,
-		}
-
-	}
-
-	handleInputText(event) {
-		this.setState({ search: event.target.value});
-	}
-
-	render () {
-		let dishes = filter(this.state.dishes, this.state.search, { key: 'name' });
-		dishes = _.sortBy(dishes, 'name');
-
-		return (
-			<div>
-				<MuiThemeProvider>
-					<div>
-						<header>
-							<Toolbar>
-								<KeyboardedInput
-									enabled
-									type={this.props.type}
-									onChange={this.handleInputText}
-									value={this.state.search}
-									min={this.props.min}
-									max={this.props.max}
-									step={this.props.step}
-									name={this.props.name}
-									className={this.props.className}
-									placeholder="Search…"
-									defaultKeyboard="kr"
-									secondaryKeyboard="us" // optional
-								/>
-								<TextField
-									id="search-input"
-									placeholder="Search…"
-									value={this.state.search}
-									onChange={this.handleInputText}
-									fullWidth
-								/>
-							</Toolbar>
-						</header>
-						<DishesList dishes={dishes} />
-					</div>
-				</MuiThemeProvider>
-				<Head>
-					<title>Meokdapedia</title>
-				</Head>
-			</div>
-		);
-	}
-}
-*/
